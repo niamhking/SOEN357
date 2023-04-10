8564f2c9da674e3fd62ee57775b9dca695021139
niamhking <78150605+niamhking@users.noreply.github.com>
2023-04-09 23:18:44 -0400

    initial layout

--- travelapp/src/App.js
+++ travelapp/src/App.js
@@ -1,15 +1,20 @@
-
-import React from 'react';
-import './App.css';
-import Navbar from './Components/Navbar/Navbar'
+import React from "react";
+import "./App.css";
+import Home from "./Components/Home/Home";
+import plane from "./planeBackground.png";
 
 const App = () => {
-  return(
-    <>
-    <Navbar/>
-    </>
-  )
-
-}
+  return (
+    <div
+      classname="colourBackground"
+      style={{
+        backgroundImage: `url(${plane})`,
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <Home></Home>
+    </div>
+  );
+};
 
 export default App;
