10f50ea4fdb2cb4db87023519bbea5147c628886
Asha I <85359230+asha97@users.noreply.github.com>
2023-04-12 03:53:44 -0400

    update of the navigation bar, created various buttons, need to fix hamburger menu

--- travelapp/src/components/Navbar.js
+++ travelapp/src/components/Navbar.js
@@ -1,24 +1,87 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
+import './Navbar.css';
+import { Button } from './Button';
+
+
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  // the handleclick is for the hamburger menu, it is going to be alternating between hamburger and 'x'
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  // if the window is huge, then the button are going to be displaying
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  // this is going to be resizing the screen and adjustment 
+  window.addEventListener('resize', showButton);
+
   return (
     <>
-    <nav className='navbar'>
+      <nav className='navbar'>
         <div className='navbar-container'>
-            {/* this is the logo */}
-            <Link to="/" className="navbar-logo"> 
-              Journi.  <i className='fab fa-typo-3'/>
-            </Link> 
-            <div className='menu-icon'>
-              {/* this is where there is going to be the hamburger menu */}
-              <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
-            </div>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            Journi.
+            <i class='fab fa-typo3' />
+          </Link>
+          {/* this is for the hamburger menu, below code */}
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          {/* there is going to be disappearing of the nav menu  */}
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/services'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/products'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Products
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to='/sign-up'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
         </div>
-    </nav>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
