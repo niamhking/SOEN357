6444b1591e90a2f9b2dfb61b50fd7d7f92559ffa
Meet Vora <meetvora02@gmail.com>
2023-04-16 18:18:45 -0400

    Recommendations Page and View Recommendation Pages
    Recommendation Pages (Frontend)

--- travelapp/src/App.js
+++ travelapp/src/App.js
@@ -2,9 +2,15 @@ import React from "react";
 import "./App.css";
 import Home from "./Components/Home/Home";
 // import plane from "./planeBackground.png";
+import Recommendations from "./Components/Recommendations/Recommendations";
 import UserConnection from "./Components/UserConnection_Timeline/UserConnection";
 import BackgroundPlane from "./Components/Background";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import EiffelTower from "./Components/Recommendations/EiffelTower";
+import LouvreMuseum from "./Components/Recommendations/LouvreMuseum";
+import ArcdeTriomphe from "./Components/Recommendations/ArcdeTriomphe";
+import Cath from "./Components/Recommendations/Cath";
+import Luxembourg from "./Components/Recommendations/Luxembourg";
 
 const App = () => {
   return (
@@ -42,6 +48,54 @@ const App = () => {
           element={
             <>
               <BackgroundPlane /> <UserConnection />
+            </>
+          }
+        />{" "}
+        <Route
+          path="/recommendations"
+          element={
+            <>
+              <BackgroundPlane /> <Recommendations />
+            </>
+          }
+        />{" "}
+        <Route
+          path="/triptoparis-eiffeltower"
+          element={
+            <>
+              <BackgroundPlane /> <EiffelTower />
+            </>
+          }
+        />{" "}
+        <Route
+          path="/triptoparis-louvremuseum"
+          element={
+            <>
+              <BackgroundPlane /> <LouvreMuseum />
+            </>
+          }
+        />{" "}
+        <Route
+          path="/triptoparis-arcdetriomphe"
+          element={
+            <>
+              <BackgroundPlane /> <ArcdeTriomphe />
+            </>
+          }
+        />{" "}
+        <Route
+          path="/triptoparis-cathedral"
+          element={
+            <>
+              <BackgroundPlane /> <Cath />
+            </>
+          }
+        />{" "}
+        <Route
+          path="/triptoparis-luxembourg"
+          element={
+            <>
+              <BackgroundPlane /> <Luxembourg />
             </>
           }
         />{" "}
