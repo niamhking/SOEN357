6903f3ef3c8c8bc26ffd264c8a78ada50a73a980
Kunal Shah <37922021+Kunal22shah@users.noreply.github.com>
2023-04-21 13:04:29 -0400

    fixing links  + consistency

--- travelapp/src/Components/Diary/PreviousJournaling.jsx
+++ travelapp/src/Components/Diary/PreviousJournaling.jsx
@@ -27,21 +27,17 @@ const PreviousJournaling = () => {
       </div>
 
       <div className="Navbar">
-        <nav class="navbar navbar-expand-lg --bs-border-color-translucent">
+         <nav class="navbar navbar-expand-lg --bs-border-color-translucent">
           <div class="container px-4 px-lg-5">
             <a class="navbar-brand" href="#!"></a>
             <div class="links">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                 <li class="nav-item">
-                  <Link to="/"class="nav-link">
+                  <Link to="/" class="nav-link">
                     Home
                   </Link>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#!">
-                    Activities
-                  </a>
-                </li>
+                <li class="nav-item"></li>
                 <li class="nav-item">
                   <Link to="/diarylist" class="nav-link">
                     Diary
@@ -49,13 +45,13 @@ const PreviousJournaling = () => {
                 </li>
                 <li class="nav-item">
                   <Link to="/timeline" class="nav-link">
-                    Timeline
+                    Connect
                   </Link>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#!">
-                    Contact
-                  </a>
+                  <Link to="/ChatBot" class="nav-link">
+                    Support
+                  </Link>
                 </li>
               </ul>
             </div>
